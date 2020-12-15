e071affe848d6142d5929bf6d4d00ae3ceda56ff
joseluisgs <joseluisgonsan@gmail.com>
2020-12-15 18:17:35 +0100

    Filtros parametros MariaDB

--- src/env.js
+++ src/env.js
@@ -9,7 +9,12 @@ import conf from 'dotenv'; // Manera ES2015 o superior usando Babel con Node
 // Cogemos el objeto que necesitamos .env
 conf.config(); // Toda la configuración parseada del fichero .env
 
-// Filtramos que estos parámetros importantes para la ejecución estén
+// Filtramos que estos parámetros importantes para la ejecución estén para MaríaDB
+const paramsMariaBD = process.env.DB_USER && process.env.DB_PASS && process.env.DB_URL && process.env.DB_PORT && process.env.DB_NAME;
+if (!paramsMariaBD) {
+  console.error('✕ Error: Faltán variables de entorno para la ejecución en MariaDB. Por favor revise su fichero .env');
+  process.exit();
+}
 
 // Es importante que pongamos unos valores por defecto por si no están en el .env o defnidos en el sistema
 module.exports = {
